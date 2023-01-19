fc7443fd3c9e139e0d31fcdeba85d2e86265a6eb
sta-wtag <122765730+sta-wtag@users.noreply.github.com>
2023-01-19 11:36:00 +0600

    Setup: project setup complete, sass added

---
+++ nuxt.config.js
@@ -0,0 +1,47 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'TodoApp',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/style-resources'
+  ],
+  styleResources: {
+    scss: [
+      '~assets/scss/variables.scss',
+      '~assets/scss/mixins.scss'
+    ]
+  },
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mainDiv">
+    <!-- <div class="card"></div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+<style lang="scss">
+  .mainDiv{
+    width: 100%;
+    height: 100vh;
+    overflow:hidden;
+    background-color: $base-color;
+  }
+   .card{
+      width: 100px;
+      height: 100px;
+      @include borderPrimary 
+      
+   }
+</style>

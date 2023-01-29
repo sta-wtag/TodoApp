6a8ddb9b7fbbdae96e003c419860a58674cab2cc
“sta-wtag” <sadia.tabassum@welldev.io>
2023-01-29 13:21:47 +0600

    fix: Resolve PR Issue

--- components/TaskCard.vue
+++ components/TaskCard.vue
@@ -1,47 +1,34 @@
 <template>
-  <div style="position: relative">
+  <div class="relative-position">
     <div v-if="loading" class="load-overlay">
       <div class="spin-icon">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="51"
-          height="51"
-          viewBox="0 0 51 51"
-          fill="none"
-        >
-          <path
-            d="M3.50011 12.6506C6.83897 6.86754 12.2301 2.73085 18.6802 1.00239C25.1305 -0.725797 31.8677 0.161127 37.6507 3.49999C41.7457 5.8642 45.0695 9.31578 47.2628 13.4815C49.3953 17.5317 50.3796 22.0842 50.1093 26.6469L45.43 26.3697C45.6498 22.6605 44.8493 18.9589 43.1151 15.6653C41.3346 12.2836 38.6346 9.48068 35.307 7.55949C25.6073 1.95934 13.1597 5.29464 7.5596 14.9944C1.95945 24.6941 5.29475 37.1416 14.9945 42.7418C18.3221 44.663 22.0995 45.5997 25.9184 45.4509C29.6378 45.3059 33.2436 44.1483 36.3461 42.1035L38.9258 46.0174C35.1095 48.5328 30.6747 49.9566 26.1009 50.1349C21.3967 50.3182 16.7457 49.1655 12.6507 46.8013C6.86766 43.4624 2.73097 38.0713 1.00251 31.6211C-0.725684 25.1709 0.161239 18.4337 3.50011 12.6506Z"
-            fill="#7A8DFD"
-          />
-        </svg>
+        <LoadingIcon />
       </div>
     </div>
     <div class="card">
-      <div v-if="!task.done" class="text-description">
-        {{ task.description }}
-      </div>
-      <div v-else class="text-description text-done">
+      <div class="text-description" :class="{ 'text-done': task.done }">
         {{ task.description }}
       </div>
       <div class="text-caption margin-top-9 margin-bottom-24">
-        Created At: {{ $helper.formatDate(task.createdAt) }}
+        {{ $t('CreatedAt') + ':  ' + formatDate }}
       </div>
       <div class="space-between flex-box">
-        <div class="flex-gap-8">
-          <div v-if="!task.done" @click="markDone()">
-            <TickIcon />
+        <div class="flex-gap-8 card-button">
+          <div v-if="!task.done" class="flex-gap-8">
+            <button @click="markDone">
+              <img src="@/assets/svg/Tick.svg" />
+            </button>
+            <button>
+              <img src="@/assets/svg/Edit.svg" />
+            </button>
           </div>
-          <div v-if="!task.done">
-            <EditIcon />
-          </div>
-          <div @click="deleteTask()">
-            <DeleteIcon />
-          </div>
+          <button @click="deleteTask">
+            <img src="@/assets/svg/Delete.svg" />
+          </button>
         </div>
-        <div v-if="task.done" class="chip textSmall">
+        <div v-if="task.done" class="chip text-small">
           <p>
-            Completed
-            {{ $helper.getDuration(task.createdAt, task.completedAt) }}
+            {{ $t('Completed') + '   ' + duration }}
           </p>
         </div>
       </div>
@@ -51,42 +38,55 @@
 <script>
 import _ from 'lodash';
 import { mapGetters } from 'vuex';
-import EditIcon from '@/components/buttons/EditIcon.vue';
-import TickIcon from '@/components/buttons/TickIcon.vue';
-import DeleteIcon from '@/components/buttons/DeleteIcon.vue';
+import LoadingIcon from '@/components/buttons/LoadingIcon.vue';
 export default {
   name: 'TaskCard',
-  components: { EditIcon, TickIcon, DeleteIcon },
+  components: { LoadingIcon },
   props: {
     cardData: {
       type: Object,
       default: null,
     },
   },
-  computed: {
-    ...mapGetters({ requestInProcess: 'todos/getCompleteRequest' }),
-  },
-
   data: () => ({
     task: null,
     loading: false,
   }),
+  computed: {
+    ...mapGetters({ requestInProcess: 'todos/getCompleteRequest' }),
+
+    formatDate() {
+      return this.$helper.formatDate(this.task.createdAt);
+    },
+    duration() {
+      return this.$helper.getDuration(
+        this.task.createdAt,
+        this.task.completedAt
+      );
+    },
+  },
 
   created() {
-    this.task = _.clone(this.cardData);
+    this.task = this.cardData;
   },
   methods: {
-    markDone() {
+    async markDone() {
       this.loading = true;
-      this.$store.dispatch('todos/changeTaskState', this.task).then(() => {
+      await this.$store
+        .dispatch('todos/changeTaskState', this.task)
+        .then(() => {
+          if (!this.requestInProcess) {
+            this.loading = false;
+          }
+        });
+    },
+    async deleteTask() {
+      this.loading = true;
+      await this.$store.dispatch('todos/deleteTask', this.task).then(() => {
         if (!this.requestInProcess) {
           this.loading = false;
-          this.task = _.clone(this.cardData);
         }
       });
-    },
-    deleteTask() {
-      this.$store.dispatch('todos/deleteTask', this.task);
     },
   },
 };
@@ -121,5 +121,13 @@ export default {
 .flex-gap-8 {
   display: flex;
   gap: 8px;
+}
+.card-button button {
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
 }
 </style>

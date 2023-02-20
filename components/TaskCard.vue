ff784f7a480b54c7d919f502df3f9cf6c43d5494
“sta-wtag” <sadia.tabassum@welldev.io>
2023-02-20 16:44:57 +0600

    fix: i18n added to toast messages

--- components/TaskCard.vue
+++ components/TaskCard.vue
@@ -149,7 +149,7 @@ export default {
       if (this.taskDescription.trim().length <= 0) {
         this.titleInputError = true;
         this.titleErrorMsg = 'Field is empty';
-        swal('Field is empty', {
+        swal('alert.message.error', {
           buttons: false,
           className: 'error',
           iconHtml: '<img src="https://picsum.photos/100/100">',
@@ -179,7 +179,7 @@ export default {
       this.alert();
     },
     alert() {
-      swal('Changes are saved successfully', {
+      swal(this.$t('alert.message.success'), {
         buttons: false,
         className: 'success',
         timer: 3000,

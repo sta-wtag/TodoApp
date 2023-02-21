b57323cdaa307595b36ec2bd56421b61edc7aa38
sta-wtag <122765730+sta-wtag@users.noreply.github.com>
2023-02-21 21:52:13 +0600

    move swal to utils

--- tests/unit/TaskCard.spec.js
+++ tests/unit/TaskCard.spec.js
@@ -1,10 +1,8 @@
 import { mount } from '@vue/test-utils';
-// import Nuxti18n from '@nuxtjs/i18n';
 import TaskCard from '@/components/TaskCard.vue';
 
 async function wrapperFactory(newState = {}) {
   const mounted = await mount(TaskCard, {
-    // Nuxti18n,
     mocks: {
       $t: (msg) => msg,
     },

be92fe500e1c391e844cd2c6fe9d1e8d46b38c43
“sta-wtag” <sadia.tabassum@welldev.io>
2023-03-02 12:38:37 +0600

    add responsiveness to splash screen

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="align-self-center content relative-position">
       <div class="icon-anim"></div>
       <div class="flex-box">
-        <SplashIcon class="align-self-center" />
+        <SplashIcon class="align-self-center icon-size" />
         <div class="splash-text margin-left-7">TODOS</div>
       </div>
     </div>
@@ -24,14 +24,90 @@ export default {
 };
 </script>
 <style lang="scss">
-.splash-text {
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 96px;
-  line-height: 112px;
-  color: $primary-text;
+@media only screen and (min-width: 1200px) {
+  .splash-text {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 96px;
+    line-height: 112px;
+    color: $primary-text;
+  }
 }
+@media only screen and (min-width: 992px) and (max-width: 1199px) {
+  .splash-text {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 96px;
+    line-height: 112px;
+    color: $primary-text;
+  }
+}
+@media only screen and (min-width: 768px) and (max-width: 991px) {
+  .splash-text {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 96px;
+    line-height: 112px;
+    color: $primary-text;
+  }
+}
+@media only screen and (max-width: 767px) and (min-width: 577px) {
+  .splash-text {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 96px;
+    line-height: 112px;
+    color: $primary-text;
+  }
+}
+
+@media only screen and (max-width: 576px) and (min-width: 376px) {
+  .icon-size {
+    width: 50px;
+  }
+  .splash-text {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 112px;
+    color: $primary-text;
+    margin-top: 20px;
+  }
+}
+@media only screen and (max-width: 375px) and (min-width: 281px) {
+  .icon-size {
+    width: 50px;
+  }
+  .splash-text {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 112px;
+    color: $primary-text;
+    margin-top: 20px;
+  }
+}
+@media only screen and (max-width: 280px) {
+  .icon-size {
+    width: 50px;
+  }
+  .splash-text {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 112px;
+    color: $primary-text;
+    margin-top: 20px;
+  }
+}
+
 .icon-anim {
   background: $base-color;
   position: absolute;
